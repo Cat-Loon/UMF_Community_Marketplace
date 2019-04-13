878568b264dea6ed107415a340edaa967a94f2bb
Cat-Loon <kaslater@umich.edu>
2019-04-13 16:15:02 -0400

    Prettying up stuff

--- app/src/main/java/com/umfmarketplace/SearchingActivity.java
+++ app/src/main/java/com/umfmarketplace/SearchingActivity.java
@@ -8,12 +8,15 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.KeyEvent;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.text.TextUtils;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
@@ -46,7 +49,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class SearchingActivity extends CreateListingDB implements View.OnClickListener{
 
     private static final String TAG = "SearchingActivity";
-    private static final String BASE_URL = "http://35.193.38.183//elasticsearch/book_listings/listing";
+    private static final String BASE_URL = "http://35.193.38.183//elasticsearch/book_listings/listing/";
     private static final int NUM_GRID_COLUMNS = 3;
     private static final int GRID_ITEM_MARGIN = 5;
 
@@ -56,6 +59,10 @@ public class SearchingActivity extends CreateListingDB implements View.OnClickLi
     private String mPrefBook;
     private String mPrefClass;
     private ArrayList<Listing> mLists;
+    private Listing List;
+    private String mListID;
+
+    private TextView mAuthor, mBook, mISBN, mPrice, mCondition, mClassUsed;
 
     private RecyclerView mRecyclerView;
     private MyAdapter mAdapter;
@@ -66,17 +73,26 @@ public class SearchingActivity extends CreateListingDB implements View.OnClickLi
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_searching);
         //Views
-        mRecyclerView = findViewById(R.id.recyclerView);
-
+        mRecyclerView = findViewById(R.id.SearchingListingRecycler);
         //Text Field
         mSearchField = findViewById(R.id.search_field);
-
         //Button
         findViewById(R.id.searchButton).setOnClickListener(this);
+
+        LinearLayoutManager manager = new LinearLayoutManager(getApplicationContext(), LinearLayoutManager.VERTICAL, false);
+        mRecyclerView.setLayoutManager(manager);
+        mRecyclerView.setHasFixedSize(true);
+
+        mAuthor = findViewById(R.id.author);
+        mBook = findViewById(R.id.book);
+        mISBN = findViewById(R.id.isbn);
+        mPrice = findViewById(R.id.price);
+        mCondition = findViewById(R.id.condition);
+        mClassUsed = findViewById(R.id.classused);
 
         getElasticPassword();
         init();
@@ -84,15 +100,17 @@ public class SearchingActivity extends CreateListingDB implements View.OnClickLi
     }
 
     private void setUpListingsList(){
+
         RecyclerViewMargin itemDecorator = new RecyclerViewMargin(GRID_ITEM_MARGIN, NUM_GRID_COLUMNS);
         mRecyclerView.addItemDecoration(itemDecorator);
         GridLayoutManager gridLayoutManager = new GridLayoutManager(SearchingActivity.this, NUM_GRID_COLUMNS);
         mRecyclerView.setLayoutManager(gridLayoutManager);
         mAdapter = new MyAdapter(mLists);
         mRecyclerView.setAdapter(mAdapter);
+
     }
 
-    private void init(){
+    private void init() {
         /*mFilters.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -101,15 +119,17 @@ public class SearchingActivity extends CreateListingDB implements View.OnClickLi
             }
         });*/
 
-        /*mSearchField.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+        getPostInfo();
+
+        mSearchField.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                if(actionId == EditorInfo.IME_ACTION_SEARCH
-                    ||actionId == EditorInfo.IME_ACTION_DONE
-                    || event.getAction() == KeyEvent.ACTION_DOWN
-                    || event.getKeyCode() == KeyEvent.KEYCODE_ENTER){
+                if (actionId == EditorInfo.IME_ACTION_SEARCH
+                        || actionId == EditorInfo.IME_ACTION_DONE
+                        || event.getAction() == KeyEvent.ACTION_DOWN
+                        || event.getKeyCode() == KeyEvent.KEYCODE_ENTER) {
 
-                    mPosts = new ArrayList<BookListing>();
+                    mLists = new ArrayList<Listing>();
 
                     Retrofit retrofit = new Retrofit.Builder()
                             .baseUrl(BASE_URL)
@@ -123,77 +143,94 @@ public class SearchingActivity extends CreateListingDB implements View.OnClickLi
 
                     String searchString = "";
 
-                    if(!mSearchField.equals("")){
-                        searchString = searchString + mSearchField.getText().toString() + "*";
-                    }
-                    if(!mPrefAuthor.equals("")){
-                        searchString = searchString + " Author:" + mPrefAuthor;
-                    }
-                    if(!mPrefBook.equals("")){
-                        searchString = searchString + " Book:" + mPrefBook;
-                    }
-                    if(!mPrefClass.equals("")){
-                        searchString = searchString + " Class:" + mPrefClass;
-                    }
-
                     Call<HitsObject> call = searchAPI.search(headerMap, "AND", searchString);
 
                     call.enqueue(new Callback<HitsObject>() {
                         @Override
                         public void onResponse(Call<HitsObject> call, Response<HitsObject> response) {
+
                             HitsList hitsList = new HitsList();
                             String jsonResponse = "";
-                            try{
-                                Log.d(TAG, "onReponse - Server Reponse: " + response.toString());
+                            try {
+                                Log.d(TAG, "onResponse: server response: " + response.toString());
 
-                                if(response.isSuccessful()){
+                                if (response.isSuccessful()) {
                                     hitsList = response.body().getHits();
                                 } else {
                                     jsonResponse = response.errorBody().string();
                                 }
 
-                                Log.d(TAG, "onResponse - hits: " + hitsList);
+                                Log.d(TAG, "onResponse: hits: " + hitsList);
 
-                                for(int i = 0; i < hitsList.getPostIndex().size(); i++){
-                                    Log.d(TAG, "onResponse - data: " + hitsList.getPostIndex().get(i).getListing().toString());
-
-                                    mPosts.add(hitsList.getPostIndex().get(i).getListing());
+                                for (int i = 0; i < hitsList.getPostIndex().size(); i++) {
+                                    Log.d(TAG, "onResponse: data: " + hitsList.getPostIndex().get(i).getListing().toString());
+                                    mLists.add(hitsList.getPostIndex().get(i).getListing());
                                 }
-                                Log.d(TAG, "onResponse - size: " + mPosts.size());
 
-                                //Set up list of Book Listings
-                            }
-                            catch(NullPointerException e){
-                                Log.e(TAG, "onResponse - NullPointerException: " + e.getMessage());
-                            }
-                            catch(IndexOutOfBoundsException e){
-                                Log.e(TAG, "onResponse - IndexOutOfBoundsException: " + e.getMessage());
-                            }
-                            catch(IOException e){
-                                Log.e(TAG, "onResponse - IOException: " + e.getMessage());
-                            }
+                                Log.d(TAG, "onResponse: size: " + mLists.size());
+                                //setup the list of posts
+                                setUpListingsList();
 
+                            } catch (NullPointerException e) {
+                                Log.e(TAG, "onResponse: NullPointerException: " + e.getMessage());
+                            } catch (IndexOutOfBoundsException e) {
+                                Log.e(TAG, "onResponse: IndexOutOfBoundsException: " + e.getMessage());
+                            } catch (IOException e) {
+                                Log.e(TAG, "onResponse: IOException: " + e.getMessage());
+                            }
                         }
 
                         @Override
                         public void onFailure(Call<HitsObject> call, Throwable t) {
                             Log.e(TAG, "onFailure: " + t.getMessage());
-                            Toast.makeText(SearchingActivity.this,"Firebase DB Search Failed", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(SearchingActivity.this, "Listing Search Failed", Toast.LENGTH_SHORT).show();
                         }
                     });
                 }
-
                 return false;
             }
-        });*/
-
-
+        });
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        getFilters();
+    private void getPostInfo(){
+        Log.d(TAG, "getPostInfo: getting the post information.");
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+
+
+        Query query = reference.child(getString(R.string.node_elastic))
+                .orderByKey()
+                .equalTo(mListID);
+
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                DataSnapshot singleSnapshot = dataSnapshot.getChildren().iterator().next();
+                if(singleSnapshot != null){
+                    List = singleSnapshot.getValue(Listing.class);
+                    Log.d(TAG, "onDataChange: found the listing: " + List.getTheAuthor());
+
+                    mAuthor.setText(List.getTheAuthor());
+                    mBook.setText(List.getTheBook());
+
+                    String price = "FREE";
+                    if(List.getPriceasString() != null){
+                        price = "$" + List.getPriceasString();
+                    }
+
+                    mPrice.setText(price);
+                    mISBN.setText(List.getISBNasString());
+                    mCondition.setText(List.getCondition());
+                    mClassUsed.setText(List.getClassUsed());
+
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 
     private void getElasticPassword(){
@@ -249,19 +286,6 @@ public class SearchingActivity extends CreateListingDB implements View.OnClickLi
 
             String searchString = "";
 
-            if(!mSearchField.equals("")){
-                searchString = searchString + mSearchField.getText().toString() + "*";
-            }
-            if(!mPrefAuthor.equals("")){
-                searchString = searchString + " Author:" + mPrefAuthor;
-            }
-            if(!mPrefBook.equals("")){
-                searchString = searchString + " Book:" + mPrefBook;
-            }
-            if(!mPrefClass.equals("")){
-                searchString = searchString + " Class:" + mPrefClass;
-            }
-
             Call<HitsObject> call = searchAPI.search(headerMap, "AND", searchString);
 
             call.enqueue(new Callback<HitsObject>() {
@@ -270,7 +294,7 @@ public class SearchingActivity extends CreateListingDB implements View.OnClickLi
                     HitsList hitsList = new HitsList();
                     String jsonResponse = "";
                     try{
-                        Log.d(TAG, "onReponse - Server Reponse: " + response.toString());
+                        Log.d(TAG, "onResponse - Server Response: " + response.toString());
 
                         if(response.isSuccessful()){
                             hitsList = response.body().getHits();
@@ -311,4 +335,5 @@ public class SearchingActivity extends CreateListingDB implements View.OnClickLi
         }
     }
 }
+
 
